5fcf11522b17676611880d89ce7cb0056df93fde
renechoi <renechoi90@gmail.com>
2023-04-10 15:23:30 +0900

    feat: authentication detail 구현

--- src/main/java/com/example/coresecurity/security/common/FormWebAuthenticationDetails.java
+++ src/main/java/com/example/coresecurity/security/common/FormWebAuthenticationDetails.java
@@ -5,20 +5,18 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Component
 public class FormWebAuthenticationDetails extends WebAuthenticationDetails {
+	private String secretKey;
+	private String something;
 
-    private  String secretKey;
-    private String something;
+	public FormWebAuthenticationDetails(HttpServletRequest request) {
+		super(request);
+		secretKey = request.getParameter("secret_key");
+		secretKey = request.getParameter("something");
+	}
 
-    public FormWebAuthenticationDetails(HttpServletRequest request) {
-        super(request);
-        secretKey = request.getParameter("secret_key");
-        something = request.getParameter("something");
-    }
+	public String getSecretKey() {
 
-    public String getSecretKey() {
-
-        return secretKey;
-    }
+		return secretKey;
+	}
 }
