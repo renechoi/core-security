9eb32558bc63bb0585cc7e0ff8f31cd6b16882ed
renechoi <renechoi90@gmail.com>
2023-04-11 22:05:45 +0900

    feat: method security

--- src/main/java/com/example/coresecurity/config/AppConfig.java
+++ src/main/java/com/example/coresecurity/config/AppConfig.java
@@ -1,23 +1,23 @@
-package com.example.coresecurity.config;
-
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-
-import com.example.coresecurity.repository.AccessIpRepository;
-import com.example.coresecurity.repository.ResourcesRepository;
-import com.example.coresecurity.security.configs.MethodSecurityConfig;
-import com.example.coresecurity.service.RoleHierarchyService;
-import com.example.coresecurity.service.SecurityResourceService;
-
-@Configuration
-@AutoConfigureBefore({MethodSecurityConfig.class})
-public class AppConfig {
-
-    @Bean
-    public SecurityResourceService securityResourceService(ResourcesRepository resourcesRepository, RoleHierarchyImpl roleHierarchy, RoleHierarchyService roleHierarchyService, AccessIpRepository accessIpRepository) {
-        SecurityResourceService SecurityResourceService = new SecurityResourceService(resourcesRepository, roleHierarchy, roleHierarchyService, accessIpRepository);
-        return SecurityResourceService;
-    }
-}
+// package com.example.coresecurity.config;
+//
+// import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+//
+// import com.example.coresecurity.repository.AccessIpRepository;
+// import com.example.coresecurity.repository.ResourcesRepository;
+// import com.example.coresecurity.security.configs.MethodSecurityConfig;
+// import com.example.coresecurity.service.RoleHierarchyService;
+// import com.example.coresecurity.service.SecurityResourceService;
+//
+// @Configuration
+// @AutoConfigureBefore({MethodSecurityConfig.class})
+// public class AppConfig {
+//
+//     @Bean
+//     public SecurityResourceService securityResourceService(ResourcesRepository resourcesRepository, RoleHierarchyImpl roleHierarchy, RoleHierarchyService roleHierarchyService, AccessIpRepository accessIpRepository) {
+//         SecurityResourceService SecurityResourceService = new SecurityResourceService(resourcesRepository, roleHierarchy, roleHierarchyService, accessIpRepository);
+//         return SecurityResourceService;
+//     }
+// }
